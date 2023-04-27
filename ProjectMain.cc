a9be33983130941852d95b9e46297e5ab2eeb48f
Alexi Stadnitski <alexi.stadnitski@helsinki.fi>
2023-04-27 22:20:55 +0300

    Backup point. Next step: program to save plots from ROOT files

--- ProjectMain.cc
+++ ProjectMain.cc
@@ -2,11 +2,13 @@
 #include "TTree.h"
 #include "Pythia8/Pythia.h"
 #include <string.h>
+
 using namespace Pythia8;
 using namespace std;
 
-#define Nevents 10000
+#define Nevents 10000 //10000
 
+// Can be simplified: return (condition AND condition AND condition)
 bool HLT_DoubleIsoMu20_eta2p1(float pT, float eta) {
     if (pT <= 20) return false;
     if (eta >= 2.1) return false;
@@ -14,8 +16,7 @@ bool HLT_DoubleIsoMu20_eta2p1(float pT, float eta) {
     return true;
 }
 
-bool HLT_DoubleIsoMu30(float pT) { return pT >= 30;}
-
+bool HLT_DoubleIsoMu30(float pT) { return pT >= 30; }
 
 void gen(string channel, TFile *events) {
 
@@ -27,10 +28,12 @@ void gen(string channel, TFile *events) {
     pythia.readString("Beams:eCM = 13600.");
 
     pythia.readString(channel + " = on");
-    if (!(strcmp(channel.c_str(), "Top:gg2ttbar"))) pythia.readString("Top:qqbar2ttbar = on");
+    if (!(strcmp(channel.c_str(), "Top:gg2ttbar"))) {
+        cout << "Compared, setting Top:qqbar2ttbar = on" << endl;
+        pythia.readString("Top:qqbar2ttbar = on");
+    }
 
     pythia.readString("25:onMode = off");
-    //pythia.readString("25:m0 = 125");
     pythia.readString("25:onIfMatch = -13 13");
     pythia.readString("Next:numberShowEvent = 0");
 
@@ -60,7 +63,7 @@ void gen(string channel, TFile *events) {
     antimuon -> Branch("antimu_charge", &antimu_charge, "antimu_charge/F");
     antimuon -> Branch("antimu_phi", &antimu_phi, "antimu_phi/F");
 
-    float trigger_efficiency1;
+    float efficiency;
     int Nmuonpassed = 0, Nantimuonpassed = 0;
     int Nmuontotal = 0, Nantimuontotal = 0;
 
@@ -71,10 +74,8 @@ void gen(string channel, TFile *events) {
         isMuon = 0;
         isAntimuon = 0;
 
-        for (int j = 0; j < pythia.event.size(); j++)
-        {
-            if (pythia.event[j].id() == -13)
-            {
+        for (int j = 0; j < pythia.event.size(); j++) {
+            if (pythia.event[j].id() == -13) {
                 isMuon = j;
                 Nmuontotal++;
 
@@ -84,18 +85,15 @@ void gen(string channel, TFile *events) {
                 transverse.fill(mu_pT);
                 pseudo.fill(mu_eta);
 
-                 if (HLT_DoubleIsoMu20_eta2p1(mu_pT, mu_eta)) 
-                 {
-                    // if (HLT_DoubleIsoMu30(mu_pT)) 
-                    // {
+                if (HLT_DoubleIsoMu20_eta2p1(mu_pT, mu_eta)) {
+
+                    //if (HLT_DoubleIsoMu30(mu_pT)) {
                         mu_m = pythia.event[isMuon].m();
                         mu_charge = pythia.event[isMuon].charge();
                         mu_phi = pythia.event[isMuon].phi();
-
                         Nmuonpassed++;
-
                         muon -> Fill();
-                    // }
+                    //}
                 } 
 
             }
@@ -111,19 +109,15 @@ void gen(string channel, TFile *events) {
                 transverse.fill(antimu_pT);
                 pseudo.fill(antimu_eta);
                  
-                 if (HLT_DoubleIsoMu20_eta2p1(antimu_pT, antimu_eta)) 
-                 {
-                    // if (HLT_DoubleIsoMu30(antimu_pT)) 
-                    // {
-
+                 if (HLT_DoubleIsoMu20_eta2p1(antimu_pT, antimu_eta)) {
+                    
+                    // if (HLT_DoubleIsoMu30(antimu_pT)) {
                         antimu_m = pythia.event[isAntimuon].m();
                         antimu_charge = pythia.event[isAntimuon].charge();
                         antimu_phi = pythia.event[isAntimuon].phi();
-
                         Nantimuonpassed++;
-
                         antimuon -> Fill();
-                    // }
+                    //}
                 } 
 
             }
@@ -131,13 +125,12 @@ void gen(string channel, TFile *events) {
             else continue;
         }
         
-        cout << endl;
     }
 
-    int Ntotal = Nmuontotal + Nantimuontotal;
-    int Npassed = Nmuonpassed + Nantimuonpassed;
+    //int Ntotal = Nmuontotal + Nantimuontotal;
+    //int Npassed = Nmuonpassed + Nantimuonpassed;
 
-    trigger_efficiency1 = 100.0*Npassed/Ntotal;
+    efficiency = 100.0 * (Nmuonpassed + Nantimuonpassed) / (Nmuontotal + Nantimuontotal);
 
     /// STEP 3: PRINT AND SAVE DATA ///
 
@@ -145,47 +138,32 @@ void gen(string channel, TFile *events) {
     cout << transverse;
     cout << pseudo;
 
-    cout << endl << "trigger_efficiency = " << trigger_efficiency1 << "%" << endl;
+    cout << endl << "Efficiency: " << efficiency << "%" << endl;
 
     muon -> Write();
     antimuon -> Write();
-
     events -> Close();
-    //exit(0);
+
 }
 
 int main() {
 
-    // TFile* events = TFile::Open("events_test.root", "RECREATE");
+    TFile* signal = TFile::Open("Root/signal.root", "RECREATE");
+    gen("HiggsSM:all", signal); // Signal (Higgs)
 
-    // TTree* signal = new TTree("signal", "Signal events generated with Pythia");
-    // TTree* DrellYan = new TTree("DrellYan", "Drell-Yan background events generated with Pythia");
-    // TTree* ttbar = new TTree("ttbar", "Ttbar background  generated with Pythia");
+    TFile* DrellYan = TFile::Open("Root/drellyan.root", "RECREATE");
+    gen("WeakSingleBoson:ffbar2gmZ", DrellYan); // Drell-Yan background
 
-    TFile* signal = TFile::Open("signal.root", "RECREATE");
-    //TFile* DrellYan = TFile::Open("DrellYan.root", "RECREATE");
-    //TFile* ttbar = TFile::Open("ttbar.root", "RECREATE");
-
-    gen("HiggsSM:all", signal); // Signal (Higgs) 
-    //gen("WeakSingleBoson:ffbar2gmZ", events, DrellYan); // Drell-Yan background 
-    //gen("Top:gg2ttbar", events, ttbar); // ttbar background #0
-
-    //gen("Top:qqbar2ttbar"); // ttbar background #1
+    TFile* ttbar = TFile::Open("Root/ttbar.root", "RECREATE"); 
+    gen("Top:gg2ttbar", ttbar); // ttbar background: function checks and turns on other channel
 
     return 0;
 
 }
 
-// == NOTES ==
-/*
-чому не прац дерева всі
-
-інвар мас
-
-селекшн 30
-
-зробити лаб 11, 12
-
-
-
+/* NOTES
+- чому не прац дерева всі
+- інвар мас
+- селекшн 30
+- зробити лаб 11, 12
 */
